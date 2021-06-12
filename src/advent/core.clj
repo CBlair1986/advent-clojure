aa2e12f856a8fddaf5e512b44b70ffffd048491f
Christopher Blair <cblair1986@gmail.com>
2021-06-12 08:14:25 -0400

    pull-input skellington

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -5,3 +5,9 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;; First I need a mechanism to pull the input files from the net, thankfully Clojure has really easy ways to do this.
+
+(defn pull-input
+  [input-number]
+  (slurp some-url))
